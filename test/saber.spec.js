cc7772fe0c19ef1e16cf99af0a3b909537fac3c7
Lior Shalev <liorshalev01@gmail.com>
2016-08-22 21:09:11 +0300

    updates

---
+++ test/saber.spec.js
@@ -0,0 +1,11 @@
+/* eslint-env mocha */
+/* eslint-disable max-len */
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+import sinon from 'sinon'
+import { expect } from 'chai'
+import { } from 'sabre'
+
+describe('Saber', function () {
+
+})
